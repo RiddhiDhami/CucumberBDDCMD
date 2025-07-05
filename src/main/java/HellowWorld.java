c445bc15af6250dd0cf4e338c9c9fe66fa51cf73
RiddhiDhami <riddhinik007@gmail.com>
2025-07-05 04:11:19 +0100

    IT-123 this is test commit

--- src/main/java/HellowWorld.java
+++ src/main/java/HellowWorld.java
@@ -1,5 +1,6 @@
 public class HellowWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("This commit from feature into master");
     }
 }
