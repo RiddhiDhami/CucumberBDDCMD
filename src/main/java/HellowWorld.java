c2a40d0df7ba7e75c9dd899fd918ccc61fae7610
Riddhi Nikunj Dhami <riddhinik007@gmail.com>
2025-07-09 00:44:18 +0100

    rebase  from intellij

--- src/main/java/HellowWorld.java
+++ src/main/java/HellowWorld.java
@@ -2,6 +2,6 @@ public class HellowWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("This commit from feature into master");
-        System.out.println("This demo is for rebase ");
+        System.out.println("This demo is for rebase from intellij");
     }
 }
