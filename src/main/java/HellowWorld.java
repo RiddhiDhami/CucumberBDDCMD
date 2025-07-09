c5cc900d9b6303a3573345c92d5084f6d09bad6c
Riddhi Nikunj Dhami <riddhinik007@gmail.com>
2025-07-10 00:06:49 +0100

    commit for build pipeline

--- src/main/java/HellowWorld.java
+++ src/main/java/HellowWorld.java
@@ -3,5 +3,6 @@ public class HellowWorld {
         System.out.println("hello world");
         System.out.println("This commit from feature into master");
         System.out.println("This demo is for rebase from intellij");
+        System.out.println("demo for buildpipeline");
     }
 }
